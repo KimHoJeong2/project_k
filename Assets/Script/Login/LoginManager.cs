47504b211512dfa058c98f13831ca4f905487a52
KimHojeong <rlaghwjd4930@gmail.com>
2021-03-19 19:21:41 +0900

    로그인 씬 작업

---
+++ Assets/Script/Login/LoginManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField] private Button m_BtnLogin = null;
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (m_BtnLogin != null)
+        {
+            Debug.Log(m_BtnLogin);
+            Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
+
+
+            m_BtnLogin.onClick.AddListener(OnClick);
+            Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
+
+            //m_BtnLogin.onClick.RemoveAllListeners();
+            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
+
+            //m_BtnLogin.onClick.AddListener(OnClick);
+            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
+
+            //m_BtnLogin.onClick.RemoveListener(OnClick);
+            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
+        }
+    }
+
+    public void OnClick()
+    {
+        Debug.LogError("OnClick");
+    }
+}
