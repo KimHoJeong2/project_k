17b00eedf6881648270b2921927bbcf3809bc41b
KimHojeong <rlaghwjd4930@gmail.com>
2021-03-25 17:10:32 +0900

    씬 작업

--- Assets/Script/Login/LoginManager.cs
+++ Assets/Script/Login/LoginManager.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 public class LoginManager : MonoBehaviour
 {
     [SerializeField] private Button m_BtnLogin = null;
-
-
+    
     private void Start()
     {
         Init();
@@ -17,26 +16,12 @@ public class LoginManager : MonoBehaviour
     {
         if (m_BtnLogin != null)
         {
-            Debug.Log(m_BtnLogin);
-            Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
-
-
             m_BtnLogin.onClick.AddListener(OnClick);
-            Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
-
-            //m_BtnLogin.onClick.RemoveAllListeners();
-            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
-
-            //m_BtnLogin.onClick.AddListener(OnClick);
-            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
-
-            //m_BtnLogin.onClick.RemoveListener(OnClick);
-            //Debug.LogError(m_BtnLogin.onClick.GetPersistentEventCount());
         }
     }
 
     public void OnClick()
     {
-        Debug.LogError("OnClick");
+        Debug.Log("OnClick");
     }
 }
